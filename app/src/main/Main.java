7f2f79a3b7a06cc815e92f658e40f6ab3c53ac3a
ItsukiArakaki <e215746@ie.u-ryukyu.ac.jp>
2022-02-08 14:39:43 +0900

    コメント追加

--- app/src/main/Main.java
+++ app/src/main/Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 public class Main {
  
 	public static void main(String[] args) {
+		// cpuの答えを取得
 		int cpuAnswer = getCpuAnswer();
  
 		while (true) {
  
+			// playerの予想値を取得
 			int playerAnswer = getPlayerAnswer();
  
+			// 正解/不正解のチェック
 			boolean judge = isJudge(cpuAnswer, playerAnswer);
  
+			// 正解の場合処理を終了、不正解の場合次のループへ
 			if (judge) {
 				break; 
 
@@ -21,12 +25,13 @@ public class Main {
 		}
 	}
  
-
+	// cpuの答えを返すメソッド
 	public static int getCpuAnswer() {
+		// 乱数を返す
 		return new Random().nextInt(100)+1;
 	}
  
-	
+	// playerが入力した数値を返すメソッド
 	public static int getPlayerAnswer() {
 		int answer;
 		
@@ -39,24 +44,25 @@ public class Main {
 				break; 
 
 			} catch (Exception e) {
+				System.out.println("数字は整数(1~100)を入力してください！");
 				i.next();
 			}
 		}
-		return answer;
+		return answer; // 入力された数値を返す
 	}
  
-	
+	// cpuの答えとplayerの答えが一致するかどうかを判定するメソッド
 	public static boolean isJudge(int cpu, int player) {
 	
-		if (cpu < player) {
+		if (cpu < player) { // 答えの方が小さい場合
 			System.out.println("もっと小さな値です！");
 			return false;
 
-		} else if (cpu > player) {
+		} else if (cpu > player) { // 答えの方が大きい場合
 			System.out.println("もっと大きな値です！");
 			return false;
 
-		} else {
+		} else { // 答えと予想値が一致した場合
 			System.out.println("正解です！！おめでとう！！");
 			return true;
 		}
